459a45855ad488042927df9967670f326c252313
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-07-20 17:50:09 +0300

    Updating images Path

--- RedMango_API/Data/ApplicationDbContext.cs
+++ RedMango_API/Data/ApplicationDbContext.cs
@@ -61,7 +61,7 @@ namespace RedMango_API.Data
                     Id = 1,
                     Name = "Spring Roll",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/spring roll.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/spring roll.jpg",
                     Price = 7.99,
                     Category = "Appetizer",
                     SpecialTag = ""
@@ -71,7 +71,7 @@ namespace RedMango_API.Data
                     Id = 2,
                     Name = "Idli",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/idli.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/idli.jpg",
                     Price = 8.99,
                     Category = "Appetizer",
                     SpecialTag = ""
@@ -81,7 +81,7 @@ namespace RedMango_API.Data
                     Id = 3,
                     Name = "Panu Puri",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque масса auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/pani puri.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/pani puri.jpg",
                     Price = 8.99,
                     Category = "Appetizer",
                     SpecialTag = "Best Seller"
@@ -91,7 +91,7 @@ namespace RedMango_API.Data
                     Id = 4,
                     Name = "Hakka Noodles",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/hakka noodles.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/hakka noodles.jpg",
                     Price = 10.99,
                     Category = "Entrée",
                     SpecialTag = ""
@@ -101,7 +101,7 @@ namespace RedMango_API.Data
                     Id = 5,
                     Name = "Malai Kofta",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/malai kofta.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/malai kofta.jpg",
                     Price = 12.99,
                     Category = "Entrée",
                     SpecialTag = "Top Rated"
@@ -111,7 +111,7 @@ namespace RedMango_API.Data
                     Id = 6,
                     Name = "Paneer Pizza",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/paneer pizza.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/paneer pizza.jpg",
                     Price = 11.99,
                     Category = "Entrée",
                     SpecialTag = ""
@@ -121,7 +121,7 @@ namespace RedMango_API.Data
                     Id = 7,
                     Name = "Paneer Tikka",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque масса auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/paneer tikka.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/paneer tikka.jpg",
                     Price = 13.99,
                     Category = "Entrée",
                     SpecialTag = "Chef's Special"
@@ -131,7 +131,7 @@ namespace RedMango_API.Data
                     Id = 8,
                     Name = "Carrot Love",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque масса auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/carrot love.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/carrot love.jpg",
                     Price = 4.99,
                     Category = "Dessert",
                     SpecialTag = ""
@@ -141,7 +141,7 @@ namespace RedMango_API.Data
                     Id = 9,
                     Name = "Rasmalai",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque масса auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/rasmalai.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/rasmalai.jpg",
                     Price = 4.99,
                     Category = "Dessert",
                     SpecialTag = "Chef's Special"
@@ -151,7 +151,7 @@ namespace RedMango_API.Data
                     Id = 10,
                     Name = "Sweet Rolls",
                     Description = "Fusc tincidunt maximus leo, sed scelerisque масса auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    Image = "https://redmangoimages.blob.core.windows.net/redmango/sweet rolls.jpg",
+                    Image = "https://redmangoimages2024.blob.core.windows.net/redmango/sweet rolls.jpg",
                     Price = 3.99,
                     Category = "Dessert",
                     SpecialTag = "Top Rated"
---
+++ RedMango_API/Migrations/20240720144122_imagesPathUpdating.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedMango_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class imagesPathUpdating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/spring roll.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/idli.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/pani puri.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/hakka noodles.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/malai kofta.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/paneer pizza.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/paneer tikka.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/carrot love.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/rasmalai.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Image",
+                value: "https://redmangoimages2024.blob.core.windows.net/redmango/sweet rolls.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/spring roll.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/idli.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/pani puri.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/hakka noodles.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/malai kofta.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/paneer pizza.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/paneer tikka.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/carrot love.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/rasmalai.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/sweet rolls.jpg");
+        }
+    }
+}
